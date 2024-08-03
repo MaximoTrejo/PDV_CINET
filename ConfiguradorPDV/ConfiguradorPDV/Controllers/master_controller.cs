9ed116774ef8ade9220cda86d8b5e78456147d05
MaximoTrejo <maximotrejo123@gmail.com>
2024-08-03 12:45:50 -0300

    fff
    fff

--- ConfiguradorPDV/ConfiguradorPDV/Controllers/master_controller.cs
+++ ConfiguradorPDV/ConfiguradorPDV/Controllers/master_controller.cs
@@ -33,7 +33,7 @@ namespace ConfiguradorPDV.Controllers
 
                 while (reader.Read())
                 {
-                    
+
                     string dbName = reader.GetString(0);
                     databases.Add(dbName);
                 }
@@ -58,49 +58,49 @@ namespace ConfiguradorPDV.Controllers
             {
                 SqlCommand comando = accesoDatos.PrepararConsulta(@"
                     DECLARE @infoCaja TABLE(
-    [caja] VARCHAR(2), 
-    [equipo] VARCHAR(100), 
-    [version] VARCHAR(100)
-);
-
-INSERT INTO @infoCaja
-SELECT DISTINCT 
-    caja, 
-    LEFT(EQUIPO, CHARINDEX('#', EQUIPO) - 1) AS equipo, 
-    valor 
-FROM (
-    SELECT 
-        RANK() OVER(
-            PARTITION BY caja, parametro
-            ORDER BY fechatrans DESC
-        ) AS rango, 
-        *
-    FROM hparamloc
-    WHERE parametro = 'VERSION'
-) AS query
-WHERE rango = 1
-ORDER BY equipo;
-
-SELECT 
-    vene_caja, 
-    suc_codigo, 
-    equipo
-FROM (
-    SELECT
-        ROW_NUMBER() OVER(
-            PARTITION BY v.vene_caja 
-            ORDER BY v.vene_fecha DESC
-        ) AS rn,
-        v.vene_caja,
-        v.suc_codigo,
-        i.equipo
-    FROM VENTAS_E v
-    INNER JOIN @infoCaja i 
-        ON v.vene_caja = i.caja COLLATE SQL_Latin1_General_CP1_CI_AS
-    WHERE v.vene_caja != ''
-) AS subquery
-WHERE rn = 1
-ORDER BY equipo;");
+                        [caja] VARCHAR(2), 
+                        [equipo] VARCHAR(100), 
+                        [version] VARCHAR(100)
+                    );
+                    
+                    INSERT INTO @infoCaja
+                    SELECT DISTINCT 
+                        caja, 
+                        LEFT(EQUIPO, CHARINDEX('#', EQUIPO) - 1) AS equipo, 
+                        valor 
+                    FROM (
+                        SELECT 
+                            RANK() OVER(
+                                PARTITION BY caja, parametro
+                                ORDER BY fechatrans DESC
+                            ) AS rango, 
+                            *
+                        FROM hparamloc
+                        WHERE parametro = 'VERSION'
+                    ) AS query
+                    WHERE rango = 1
+                    ORDER BY equipo;
+                    
+                    SELECT 
+                        vene_caja, 
+                        suc_codigo, 
+                        equipo
+                    FROM (
+                        SELECT
+                            ROW_NUMBER() OVER(
+                                PARTITION BY v.vene_caja 
+                                ORDER BY v.vene_fecha DESC
+                            ) AS rn,
+                            v.vene_caja,
+                            v.suc_codigo,
+                            i.equipo
+                        FROM VENTAS_E v
+                        INNER JOIN @infoCaja i 
+                            ON v.vene_caja = i.caja COLLATE SQL_Latin1_General_CP1_CI_AS
+                        WHERE v.vene_caja != ''
+                    ) AS subquery
+                    WHERE rn = 1
+                    ORDER BY equipo;");
 
                 SqlDataReader reader = comando.ExecuteReader();
                 dataTable.Load(reader);
@@ -109,7 +109,7 @@ ORDER BY equipo;");
             catch (Exception ex)
             {
                 MessageBox.Show("No se encontro equipos " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-               
+
             }
 
             return dataTable;
